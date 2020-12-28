6c1c8f78e540fde67586197e3b8a2eb933f99899
Ezequiel Veliz <ezequiel_veliz@hotmail.com>
2020-12-28 10:37:25 -0300

    * Migrations draft.

--- migration/Migration.js
+++ migration/Migration.js
@@ -1,1 +1,36 @@
-// Futuras migraciones para instanciar dbs
+class Migration {
+
+    set #name(value) {
+        this.#_name = value;
+    }
+
+    get #version() {
+        return this.#_version;
+    }
+
+    set #version(value) {
+        this.#_version = value;
+    }
+    #_name;
+    #_version;
+    #_migrations = [];
+
+    constructor(name, version, migration) {
+        this.#name = name;
+        this.#version = version;
+        this.#_migrations.push(migration);
+        return this;
+    }
+
+    add(migration) {
+        this.#version ++;
+        this.#_migrations.push(migration);
+        return this;
+    }
+
+    run() {}
+}
+
+class MigrationVersion {
+    
+}
